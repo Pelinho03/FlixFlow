884b3cb318f77eeccb25eb53c72c0b7aa7475437
Paulo Guimarães <elmuchacho@outlook.pt>
2024-12-17 12:05:40 +0000

    improve design favorite page

--- flixflow/lib/screens/favorites_page.dart
+++ flixflow/lib/screens/favorites_page.dart
@@ -86,12 +86,13 @@ class _FavoritePageState extends State<FavoritePage> {
 
           // Exibindo os filmes em duas colunas
           return GridView.builder(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 45), // Espaçamento lateral
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, // Duas colunas
-              crossAxisSpacing: 12, // Espaço entre as colunas
-              mainAxisSpacing: 1, // Espaço entre as linhas
-              childAspectRatio:
-                  0.70, // Ajuste de proporção para que a imagem seja mais alta do que larga
+              crossAxisSpacing: 3, // Espaçamento horizontal entre colunas
+              mainAxisSpacing: 16, // Espaçamento vertical entre linhas
+              childAspectRatio: 0.9, // Controla a proporção largura/altura
             ),
             itemCount: favoriteMovies.length,
             itemBuilder: (context, index) {
@@ -112,81 +113,83 @@ class _FavoritePageState extends State<FavoritePage> {
 
   // Método para construir cada item de filme
   Widget _buildFavoriteTile(dynamic movie) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 70.0, horizontal: 12.0),
-      child: Stack(
-        alignment: Alignment.bottomRight, // Posicionamento do ícone
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MovieDetailPage(movie: movie),
-                ),
-              );
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: movie['poster_path'] != null
-                        ? Image.network(
-                            'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
-                            fit: BoxFit.cover,
-                            width: 148,
-                            height: 184,
-                          )
-                        : const Icon(Icons.movie, size: 80),
-                  ),
-                ),
-                const SizedBox(height: 11.0),
-                Text(
-                  movie['title'] ?? 'Título não disponível',
-                  style: AppTextStyles.mediumText.copyWith(
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MovieDetailPage(movie: movie),
+              ),
+            );
+          },
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: movie['poster_path'] != null
+                    ? Image.network(
+                        'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
+                        fit: BoxFit.cover,
+                        width: 148,
+                        height: 180, // Altura ajustável
+                      )
+                    : const Icon(Icons.movie, size: 80),
+              ),
+              Positioned(
+                bottom: 4,
+                right: 4,
+                child: Container(
+                  decoration: BoxDecoration(
                     color: AppColors.primeiroPlano,
+                    borderRadius: BorderRadius.circular(50),
                   ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  '${movie['release_date']?.substring(0, 4) ?? 'N/A'}',
-                  style: AppTextStyles.smallText.copyWith(
-                    color: AppColors.roxo,
+                  child: FutureBuilder<bool>(
+                    future: isFavorite(movie),
+                    builder: (context, snapshot) {
+                      final isFav = snapshot.data ?? false;
+                      return IconButton(
+                        icon: Icon(
+                          isFav ? Icons.favorite : Icons.favorite_border,
+                          color: isFav ? AppColors.roxo : AppColors.cinza,
+                        ),
+                        onPressed: () async {
+                          await toggleFavorite(movie);
+                        },
+                      );
+                    },
                   ),
                 ),
-              ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        // Título do filme
+        SizedBox(
+          width: 148,
+          child: Text(
+            movie['title'] ?? 'Título não disponível',
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.primeiroPlano,
             ),
+            maxLines: 1, // Limita a uma linha
+            overflow: TextOverflow.ellipsis, // Trunca o texto se necessário
           ),
-          // Ícone de favoritos no canto inferior direito, mas sobre a imagem
-          FutureBuilder<bool>(
-            future: isFavorite(movie),
-            builder: (context, snapshot) {
-              final isFav = snapshot.data ?? false;
-
-              return Container(
-                decoration: BoxDecoration(
-                  color: AppColors.primeiroPlano.withOpacity(
-                      0.6), // Transparência para não cobrir totalmente a imagem
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: IconButton(
-                  icon: Icon(
-                    isFav ? Icons.favorite : Icons.favorite_border,
-                    color: isFav ? AppColors.roxo : AppColors.cinza,
-                    size: 30,
-                  ),
-                  onPressed: () async {
-                    await toggleFavorite(movie); // Alterna estado do favorito
-                  },
-                ),
-              );
-            },
+        ),
+        SizedBox(
+          width: 148,
+          child: Text(
+            '${movie['release_date']?.substring(0, 4) ?? 'N/A'}',
+            style: AppTextStyles.smallText.copyWith(
+              color: AppColors.roxo,
+            ),
+            maxLines: 1, // Limita a uma linha
+            overflow: TextOverflow.ellipsis,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
