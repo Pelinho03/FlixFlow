b29d92b736b06f63fe21932434365b47989f583b
Paulo Guimarães <elmuchacho@outlook.pt>
2025-01-30 22:42:46 +0000

    update logout

--- flixflow/lib/services/auth_service.dart
+++ flixflow/lib/services/auth_service.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flixflow/styles/app_colors.dart';
+import 'package:flixflow/styles/app_text.dart';
 
 class AuthService {
-  // Exibe um diálogo para confirmar o logout
+  // Exibe um diálogo para confirmar o logout com o estilo desejado
   static Future<bool> confirmLogout(BuildContext context) async {
     final result = await showDialog<bool>(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text("Confirmação de Logout"),
-          content: const Text("Tens a certeza que queres sair?"),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text("Cancelar"),
+          title: Center(
+            child: Text(
+              "Confirmação de Logout",
+              style: AppTextStyles.bigText.copyWith(color: AppColors.vermelho),
             ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: const Text("Sair"),
+          ),
+          content: Text(
+            "Tens a certeza que queres sair?",
+            style: AppTextStyles.mediumText
+                .copyWith(color: AppColors.primeiroPlano),
+            textAlign: TextAlign
+                .center, // Opcional para garantir o alinhamento centralizado
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: Text(
+                    "Cancelar",
+                    style: AppTextStyles.mediumText
+                        .copyWith(color: AppColors.roxo),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: Text(
+                    "Sair",
+                    style: AppTextStyles.mediumText
+                        .copyWith(color: AppColors.roxo),
+                  ),
+                ),
+              ],
             ),
           ],
         );
