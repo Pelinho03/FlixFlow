97fe299d35a70d7bbd610451d97992a98f58bd1f
Paulo Guimarães <elmuchacho@outlook.pt>
2025-02-01 18:52:40 +0000

    banner fix

--- flixflow/lib/widgets/search_banner_widget.dart
+++ flixflow/lib/widgets/search_banner_widget.dart
@@ -9,41 +9,53 @@ class SearchBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 40),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/imgs/banner_modify.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/imgs/login_logo.png',
-            height: 35,
-            fit: BoxFit.contain,
-          ),
-          const SizedBox(height: 12),
-          TextField(
-            autofocus: false,
-            onChanged: onSearchChanged,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: AppColors.primeiroPlano,
-              hintText: 'Procurar filme',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(50),
-                borderSide: BorderSide.none,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SizedBox(
+          width: double.infinity, // Ocupar a largura total disponível
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth:
+                  constraints.maxWidth, // Não ultrapassa a largura do ecrã
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/imgs/teste.png'),
+                fit: BoxFit.cover, // Ajusta a imagem sem distorcer
               ),
-              prefixIcon: const Icon(Icons.search, color: AppColors.cinza),
             ),
-            style: AppTextStyles.mediumText.copyWith(
-              color: AppColors.cinza,
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/imgs/login_logo.png',
+                  height: 35,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 12),
+                TextField(
+                  autofocus: false,
+                  onChanged: onSearchChanged,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.primeiroPlano,
+                    hintText: 'Procurar filme',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon:
+                        const Icon(Icons.search, color: AppColors.cinza),
+                  ),
+                  style: AppTextStyles.mediumText.copyWith(
+                    color: AppColors.cinza,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
